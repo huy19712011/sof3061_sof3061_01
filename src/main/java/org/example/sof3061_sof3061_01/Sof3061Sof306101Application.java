882d3ecd8f68ce70a0c3147f724924a14d749b34
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-04-03 19:00:10 +0700

    Set up Spring Boot project

---
+++ src/main/java/org/example/sof3061_sof3061_01/Sof3061Sof306101Application.java
@@ -0,0 +1,13 @@
+package org.example.sof3061_sof3061_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof3061Sof306101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sof3061Sof306101Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/sof3061_sof3061_01/Sof3061Sof306101ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sof3061_sof3061_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof3061Sof306101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
